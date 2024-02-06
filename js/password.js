248e4dee663d1fcef74f7c3b6fa8a444df6aaf40
webjn <159195777+webjn@users.noreply.github.com>
2024-02-06 23:33:01 +0200

    Add files via upload

---
+++ js/password.js
@@ -0,0 +1,15 @@
+// Correct password
+const correctPassword = "123";
+
+// Function to check the entered password
+function checkPassword() {
+  const passwordInput = document.getElementById("passwordInput").value;
+  if (passwordInput === correctPassword) {
+    // If password is correct, show the content
+    document.getElementById("passwordPrompt").style.display = "none";
+    document.getElementById("content").classList.remove("hidden");
+  } else {
+    // If password is incorrect, show an alert
+    alert("Incorrect password! Please try again.");
+  }
+}
