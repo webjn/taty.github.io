914c2123f562bf53006ae7e1ac635f9f216ca872
webjn <159195777+webjn@users.noreply.github.com>
2024-02-12 19:23:00 +0200

    Update password-1.js

--- js/password-1.js
+++ js/password-1.js
@@ -1,5 +1,5 @@
 // Correct password
-const correctPassword = "##1Taty1##@Joe@20223";
+const correctPassword = "##1Taty1##@Joe@2023";
 
 // Function to check the entered password
 function checkPassword() {
